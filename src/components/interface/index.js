767d43ddbbd5878ac3c8349691a41f655f5bb2df
williamprt <williampp2015@gmail.com>
2019-08-18 00:39:36 -0400

    create an input and a button that works without state manager for interface component

--- src/components/interface/index.js
+++ src/components/interface/index.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 
 class Interface extends Component {
+    state = {
+        message: [],
+        newMessage: '',
+    }
+
     render() {
-        return <h1>its the interface component</h1>
+        return (
+            <div className="InterfaceMain">
+                <section>
+                    <article>
+                        { this.state.message.map(message => (
+                            <li key={message.id}>{message.text}</li>
+                        )) }
+                    </article>
+
+                    <article>
+                        <input 
+                            value={this.state.newMessage}
+                            onChange={(e) => this.setState({ newMessage: e.target.value })}
+                        />
+                        <button onClick={() => {
+                            let messages_array = this.state.message;
+                            let new_content = this.state.newMessage;
+                            this.setState(
+                                { message: [...messages_array, {id: Math.random(), text: new_content}] }
+                            );
+                            this.setState({ newMessage: '' });
+                        }}>Send Message</button>
+                    </article>
+                </section>
+            </div>
+        )
     }
 }
 
